5fae100e4b02206bb5a2d4c86036b1e363ceb470
lunaboppin <20017802+lunaboppin@users.noreply.github.com>
2026-08-30 00:34:24 +0000

    Provide Metrostroi time fallback on clients

--- gamemode/metrostroi_entry_cl.lua
+++ gamemode/metrostroi_entry_cl.lua
@@ -1,6 +1,19 @@
 if Metrostroi then
-    print("[metro] Metrostroi global already present, workshop scripts addon has loaded, skipping vendored copy")
-    return
+	Metrostroi.GetTimedT = Metrostroi.GetTimedT or function(notsync)
+		local T0 = GetGlobalFloat("MetrostroiT0", os.time()) + GetGlobalFloat("MetrostroiTY")
+		local T1 = GetGlobalFloat("MetrostroiT1", CurTime())
+
+		if notsync then
+			return (os.time() - T0) - (CurTime() - T1)
+		end
+
+		return (os.time() - T0 + (CurTime() % 1.0)) - (CurTime() - T1)
+	end
+	Metrostroi.GetSyncTime = Metrostroi.GetSyncTime or function(notsync)
+		return os.time() - Metrostroi.GetTimedT(notsync)
+	end
+	print("[metro] Metrostroi global already present, workshop scripts addon has loaded, skipping vendored copy")
+	return
 end
 
 local rawFileFind = file.Find
